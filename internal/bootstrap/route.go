7cf4b0c2de8433ed07b37a32a4479dd28a868bae
ragil <fsparatos@gmail.com>
2026-09-11 02:27:38 +0000

    feat: merubah kata- kata hari ini

--- internal/bootstrap/route.go
+++ internal/bootstrap/route.go
@@ -11,7 +11,7 @@ import (
 
 func registerRoutes(app *fiber.App, db *gorm.DB) {
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"message": "Hello, welcome to the api after deploy with workflow github actions"})
+		return c.JSON(fiber.Map{"message": "Hello, welcome to the api after deploy with workflow github"})
 	})
 
 	app.Get("/hello", func(c *fiber.Ctx) error {
